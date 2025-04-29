c3b881a7a023e51c316861c2cae6d3cc9fbee7f6
leonardoalta <leonardoalta7688@gmail.com>
2025-04-28 23:27:20 -0600

    prueba1

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ console.log(process.env);
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
   production: {
     url: process.env.DATABASE_URL,
-    dialect: 'mysql',
+    dialect: 'postgres',
   }
 };
 
